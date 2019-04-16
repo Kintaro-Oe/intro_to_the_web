bda9fc2722c22db4e63a3ffc8c7e21a5f98c650c
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-16 16:37:29 +0100

    add name form

--- app.rb
+++ app.rb
@@ -14,6 +14,13 @@ get '/another' do
   'This is the third route!'
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
